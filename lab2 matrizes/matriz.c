0b134eee3b1fb0b0ee6f4dd4b7782e7398e086f0
HenriqueLay <henriquelayber@gmail.com>
2019-04-03 15:46:27 -0300

    Inicializando sem checkagem

--- lab2 matrizes/matriz.c
+++ lab2 matrizes/matriz.c
@@ -17,14 +17,10 @@ Matriz* inicializaMatriz (int nlinhas, int ncolunas){
     Matriz *mat = (Matriz*) malloc(sizeof(Matriz));
     mat->nLinhas = nlinhas;
     mat->nColunas = ncolunas;
-    mat->matriz = (int**) malloc(sizeof(int*) * mat->nLinhas);
+    mat->matriz = (int**) malloc(sizeof(int*) * mat->nLinhas);  
+
     for(int i = 0; i < mat->nColunas; i++)
         mat->matriz[i] = (int*) malloc(sizeof(int) * mat->nColunas);
-
-    if(mat->matriz[0][0] == NULL){
-        puts("Deu algo de errado ao alocar a matriz.");
-        EXIT_FAILURE;
-    }    
 
     return mat;
 }
